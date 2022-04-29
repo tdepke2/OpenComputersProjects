0aa92e35cc3dcf17b50c07122fc6eb843f0c0c8e
Thomas Depke <tommydepke@gmail.com>
2022-04-28 23:09:50 -0600

    Refactor instances of 'library' to 'module' in include, it's a module loader after all.

--- libapptools/include.lua
+++ libapptools/include.lua
@@ -1,13 +1,13 @@
 --[[
-Library loader (improved version of require() function).
+Module loader (improved version of require() function).
 
-Extends the functionality of require() to solve an issue with libraries getting
-cached and not reloading. The include() function will reload a library if it
-detects that the file has been modified. Any user libraries that need to be
-loaded should use include() instead of require() for this to work properly
-(except for the include module itself). Using include() for system libraries is
-not necessary and may not always work. Take a look at the package.loaded
-warnings here: https://ocdoc.cil.li/api:non-standard-lua-libs
+Extends the functionality of require() to solve an issue with modules getting
+cached and not reloading. The include() function will reload a module if it
+detects that the file has been modified. Any user modules that need to be loaded
+should use include() instead of require() for this to work properly (except for
+the include module itself). Using include() for system libraries is not
+necessary and may not always work. Take a look at the package.loaded warnings
+here: https://ocdoc.cil.li/api:non-standard-lua-libs
 
 In order for include() to work properly, a module must meet these requirements:
   1. It must return a table.
@@ -48,7 +48,7 @@ include.loaded = {}
 -- Performs a move (copy) operation from table src into table dest, this allows
 -- changing the memory address of the original table. If the dest table has a
 -- metatable with the __metatable field defined, an error is raised. If the src
--- table has a reference back to itself, and error is raised as this would imply
+-- table has a reference back to itself, an error is raised as this would imply
 -- that the reference was not fully moved.
 local function moveTableReference(src, dest)
   -- Clear contents of dest.
@@ -85,86 +85,86 @@ local function moveTableReference(src, dest)
 end
 
 
--- include.load(libraryName: string): table
+-- include.load(moduleName: string): table
 -- 
--- Loads a library just like require() does. The difference is that the library
+-- Loads a module just like require() does. The difference is that the module
 -- will be removed from the internal cache and loaded again if the file modified
 -- time changes. This helps resolve the annoying problem where a change is made
--- in a library, the library is included with require() in a source file, and
--- the changes made do not show up during testing (because library has been
+-- in a module, the module is included with require() in a source file, and the
+-- changes made do not show up during testing (because the module has been
 -- cached). Normally you would have to either reboot the machine to force the
--- library to reload, or remove the entry in package.loaded manually. The
+-- module to reload, or remove the entry in package.loaded manually. The
 -- include.load() function fixes this problem.
-function include.load(libraryName)
-  local libraryPath = package.searchpath(libraryName, package.path)
-  assert(libraryPath and filesystem.exists(libraryPath), "Cannot find library \"" .. libraryName .. "\" in search path.")
-  local modifiedTime = filesystem.lastModified(libraryPath)
-  local lib
+function include.load(moduleName)
+  local modulePath = package.searchpath(moduleName, package.path)
+  assert(modulePath and filesystem.exists(modulePath), "Cannot find module \"" .. moduleName .. "\" in search path.")
+  local modifiedTime = filesystem.lastModified(modulePath)
+  local mod
   
-  -- Attempt to reload library if it doesn't appear in package, we don't know the modification time, or modification time changed.
-  if not package.loaded[libraryName] or (include.loaded[libraryName] or -1) ~= modifiedTime then
-    if libraryName == "dlog" then
-      io.write("Include is reloading lib dlog, was found in package = " .. tostring(package.loaded[libraryName]) .. "\n")
+  -- Attempt to reload module if it doesn't appear in package, we don't know the modification time, or modification time changed.
+  if not package.loaded[moduleName] or (include.loaded[moduleName] or -1) ~= modifiedTime then
+    if moduleName == "dlog" then
+      io.write("Include is reloading mod dlog, was found in package = " .. tostring(package.loaded[moduleName]) .. "\n")
     end
     
-    print("Reloading library " .. libraryName)
-    local oldModule = package.loaded[libraryName]
-    package.loaded[libraryName] = nil
-    lib = require(libraryName)
-    include.loaded[libraryName] = modifiedTime
+    print("Reloading module " .. moduleName)
+    local oldModule = package.loaded[moduleName]
+    package.loaded[moduleName] = nil
+    mod = require(moduleName)
+    include.loaded[moduleName] = modifiedTime
     
     -- If this is not the first time the module loaded, we need to purge the newly created one and swap the contents into the previous module.
     if oldModule then
-      moveTableReference(lib, oldModule)
-      package.loaded[libraryName] = oldModule
+      moveTableReference(mod, oldModule)
+      package.loaded[moduleName] = oldModule
     end
   else
-    if libraryName == "dlog" then
-      io.write("Include found lib dlog already\n")
+    if moduleName == "dlog" then
+      io.write("Include found mod dlog already\n")
     end
     
-    print("Keeping library " .. libraryName)
-    lib = package.loaded[libraryName]
+    print("Keeping module " .. moduleName)
+    mod = package.loaded[moduleName]
   end
   
-  if libraryName == "dlog" then
-    io.write("dlog lib = " .. tostring(lib) .. "\n")
+  if moduleName == "dlog" then
+    io.write("dlog mod = " .. tostring(mod) .. "\n")
   end
   
-  return lib
+  return mod
 end
 
 
--- include.isLoaded(libraryName: string): boolean
+-- include.isLoaded(moduleName: string): boolean
 -- 
--- Check if a library is currently loaded (already in cache).
-function include.isLoaded(libraryName)
-  return package.loaded[libraryName] ~= nil
+-- Check if a module is currently loaded (already in cache).
+function include.isLoaded(moduleName)
+  return package.loaded[moduleName] ~= nil
 end
 
 
--- include.reload(libraryName: string): table
+-- include.reload(moduleName: string): table
 -- 
--- Forces a library to load/reload, regardless of the file modification time. Be
+-- Forces a module to load/reload, regardless of the file modification time. Be
 -- careful not to use this with system libraries!
-function include.reload(libraryName)
-  package.loaded[libraryName] = nil
-  return include.load(libraryName)
+function include.reload(moduleName)
+  package.loaded[moduleName] = nil
+  return include.load(moduleName)
 end
 
 
--- include.unload(libraryName: string)
+-- include.unload(moduleName: string)
 -- 
--- Unloads the given library (removes it from the internal cache). Be careful
--- not to use this with system libraries!
-function include.unload(libraryName)
-  package.loaded[libraryName] = nil
+-- Unloads the given module (removes it from the internal cache). Be careful not
+-- to use this with system libraries!
+function include.unload(moduleName)
+  package.loaded[moduleName] = nil
 end
 
 
 -- include.unloadAll()
 -- 
--- Unloads all libraries that have been loaded with include(), include.load(),
+-- Unloads all modules that have been loaded with include(), include.load(),
 -- include.reload(), etc. System libraries will not be touched as long as they
 -- were loaded through other means, like require().
 function include.unloadAll()
@@ -175,7 +175,7 @@ function include.unloadAll()
 end
 
 
--- include.iterateSrcDependencies(sourceFilename: string[, libPattern: string]):
+-- include.iterateSrcDependencies(sourceFilename: string[, modPattern: string]):
 --   function
 -- 
 -- Gets an iterator to walk through the library dependencies for a source code
@@ -186,23 +186,23 @@ end
 -- to the root of the source tree. This means each file will at most depend on
 -- previous returned files or itself.
 -- 
--- The sourceFilename is the path to the source code file, libPattern is a
+-- The sourceFilename is the path to the source code file, modPattern is a
 -- pattern for the require() function equivalent. With the default value for
--- libPattern, the strings 'require("")' and 'include("")' will be scanned for
+-- modPattern, the strings 'require("")' and 'include("")' will be scanned for
 -- to find nested libraries in source code. Note that the nested libraries will
 -- be searched by package name, not by file path. Also, the include module
 -- itself is blacklisted from getting picked up as a dependency (to prevent some
 -- complications).
 -- 
--- For each call to the iterator, returns the library name (string) and contents
--- of the source file (also string). The library name will be an empty string if
+-- For each call to the iterator, returns the module name (string) and contents
+-- of the source file (also string). The module name will be an empty string if
 -- the source file corresponds to the original sourceFilename argument. Iterator
 -- returns nil after last source file has been returned.
-function include.iterateSrcDependencies(sourceFilename, libPattern)
-  libPattern = libPattern or "require%(\"([^\"]*)\"%)"
+function include.iterateSrcDependencies(sourceFilename, modPattern)
+  modPattern = modPattern or "require%(\"([^\"]*)\"%)"
   local srcStack = {sourceFilename}
-  local searchedLibs = {include=true}
-  local sentLibs = {}
+  local searchedMods = {include=true}
+  local sentMods = {}
   
   local function srcIter()
     if not srcStack then
@@ -210,7 +210,7 @@ function include.iterateSrcDependencies(sourceFilename, libPattern)
     end
     -- Get source file on top of stack that hasn't been sent yet.
     local srcTop = srcStack[#srcStack]
-    while sentLibs[srcTop] do
+    while sentMods[srcTop] do
       srcStack[#srcStack] = nil
       srcTop = srcStack[#srcStack]
     end
@@ -227,27 +227,27 @@ function include.iterateSrcDependencies(sourceFilename, libPattern)
     local srcCode = srcFile:read("*a")
     srcFile:close()
     
-    -- Look for the libPattern or 'include("")' strings. Only count ones we haven't searched before (to protect against circular dependencies).
-    searchedLibs[srcTop] = true
-    local hasNewLibs = false
-    for libName in string.gmatch(srcCode, libPattern) do
-      if not searchedLibs[libName] then
-        srcStack[#srcStack + 1] = libName
-        hasNewLibs = true
+    -- Look for the modPattern or 'include("")' strings. Only count ones we haven't searched before (to protect against circular dependencies).
+    searchedMods[srcTop] = true
+    local hasNewMods = false
+    for modName in string.gmatch(srcCode, modPattern) do
+      if not searchedMods[modName] then
+        srcStack[#srcStack + 1] = modName
+        hasNewMods = true
       end
     end
-    for libName in string.gmatch(srcCode, "include%(\"([^\"]*)\"%)") do
-      if not searchedLibs[libName] then
-        srcStack[#srcStack + 1] = libName
-        hasNewLibs = true
+    for modName in string.gmatch(srcCode, "include%(\"([^\"]*)\"%)") do
+      if not searchedMods[modName] then
+        srcStack[#srcStack + 1] = modName
+        hasNewMods = true
       end
     end
     
-    if hasNewLibs then
+    if hasNewMods then
       return srcIter()
     elseif #srcStack > 1 then
       srcStack[#srcStack] = nil
-      sentLibs[srcTop] = true
+      sentMods[srcTop] = true
       return srcTop, srcCode
     else
       srcStack = nil
