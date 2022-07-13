68266e9510b99d7ac4c03f5c65846be609229495
Thomas Depke <tommydepke@gmail.com>
2022-07-13 00:40:00 -0600

    Add command line options to simple_preprocess and Bakefile for automating build.

--- libmnet/simple_preprocess.lua
+++ libmnet/simple_preprocess.lua
@@ -1,11 +1,37 @@
+--------------------------------------------------------------------------------
+-- Minimalistic Lua preprocessor.
+-- 
+-- simple_preprocess [input file] [output file] [-h] [--help] [--local-env={}]
+-- 
+-- @author tdepke2
+--------------------------------------------------------------------------------
+
+
+-- FIXME do we want a man page? #############################################################
+-- need to explain format of # lines, the special spwrite function, and example code.
+-- If an input or output filename is unspecified or replaced with a dash, it will default to stdin or stdout respectively.
 
 
 
 local fs = require("filesystem")
+local serialization = require("serialization")
 local shell = require("shell")
 
-local args = shell.parse(...)
+local args, opts = shell.parse(...)
 
+
+local function printUsage()
+  io.write([[Usage: simple_preprocess [OPTION]... [INPUT-FILE] [OUTPUT-FILE]
+
+Options:
+  -h, --help                display help message and exit
+  -v, --verbose             print additional debug information to stdout
+      --local-env=STRING    data to append to local environment when processing
+                            input (format should use serialization library)
+
+For more information, run: man simple_preprocess
+]])
+end
 
 local function checkFileError(filename, file, err)
   if not file then
@@ -19,42 +45,58 @@ local function checkFileError(filename, file, err)
 end
 
 local function main()
-  if args[1] == nil then
-    args[1] = "-"
+  local inputFilename = (args[1] == nil and "-" or args[1])
+  local outputFilename = (args[2] == nil and "-" or args[2])
+  if #args > 2 or opts["h"] or opts["help"] then
+    printUsage()
+    os.exit(0)
   end
-  if args[2] == nil then
-    args[2] = "-"
+  local verbose = (opts["v"] or opts["verbose"])
+  local appendEnv
+  if opts["local-env"] then
+    appendEnv = serialization.unserialize(tostring(opts["local-env"]))
+    if type(appendEnv) ~= "table" then
+      io.stderr:write("simple_preprocess: provided --local-env argument could not be deserialized to a table value\n")
+      os.exit(1)
+    end
   end
+  
   local inputFile, outputFile, err
   
   -- Attempt to open input file (or use stdin).
-  if args[1] == "-" then
+  if inputFilename == "-" then
     inputFile, err = io.stdin, "missing stdin"
   else
-    args[1] = shell.resolve(args[1])
-    inputFile, err = io.open(args[1])
+    inputFilename = shell.resolve(inputFilename)
+    inputFile, err = io.open(inputFilename)
   end
-  checkFileError(args[1], inputFile, err)
+  checkFileError(inputFilename, inputFile, err)
   
   -- Attempt to open output file (or use stdout).
-  if args[2] == "-" then
+  if outputFilename == "-" then
     outputFile, err = io.stdout, "missing stdout"
   else
-    args[2] = shell.resolve(args[2])
-    outputFile, err = io.open(args[2], "w")
+    outputFilename = shell.resolve(outputFilename)
+    outputFile, err = io.open(outputFilename, "w")
   end
-  checkFileError(args[2], outputFile, err)
+  checkFileError(outputFilename, outputFile, err)
+  
+  -- 
+  local outputLines, outputLineNum = {}, 1
+  local function spwrite(...)
+    outputLines[outputLineNum] = string.rep("%s", select("#", ...)):format(...) .. "\n"
+    outputLineNum = outputLineNum + 1
+  end
   
   -- Iterate each line in the file.
-  local processedLines = {}
-  local line, lineNum = inputFile:read(), 1
+  local processedLines, lineNum = {}, 1
+  local line = inputFile:read()
   while line do
-    outputFile:write("[" .. line .. "]\n")
     local firstNumberSign = string.find(line, "#", 1, true)
     if firstNumberSign and string.match(line, "^%s*#[^#]") then
       processedLines[lineNum] = string.sub(line, 1, firstNumberSign - 1) .. string.sub(line, firstNumberSign + 1) .. "\n"
     else
-      processedLines[lineNum] = string.format("io.write(%q)\n", line .. "\n")
+      processedLines[lineNum] = string.format("spwrite(%q)\n", line)
     end
     line, lineNum = inputFile:read(), lineNum + 1
   end
@@ -62,8 +104,11 @@ local function main()
   
   local y = true
   
-  outputFile:write("RUNNING LOAD...\n")
-  
+  if verbose then
+    io.write("################# PREPARED FILE ##################\n")
+    io.write(table.concat(processedLines))
+    io.write("(END)\n")
+  end
   
   
   -- TODO: just got env working, need to modify so that output goes to another table that we write to the file (write one line at a time).
@@ -76,17 +121,27 @@ local function main()
   local fn, result = load(function()
     lineNum = lineNum + 1
     return processedLines[lineNum]
-  end, "=(load)", "t", setmetatable({y=y}, {
+  end, "=(load)", "t", setmetatable({spwrite=spwrite, y=y}, {
     __index = _ENV
   }))
   if not fn then
-    io.stderr:write("simple_preprocess " .. args[1] .. ": " .. tostring(result) .. "\n")
+    io.stderr:write("simple_preprocess " .. inputFilename .. ": " .. tostring(result) .. "\n")
     os.exit(1)
   end
   local status, result = pcall(fn)
   if not status then
-    io.stderr:write("simple_preprocess " .. args[1] .. ": " .. tostring(result) .. "\n")
+    io.stderr:write("simple_preprocess " .. inputFilename .. ": " .. tostring(result) .. "\n")
     os.exit(1)
+  end
+  
+  if verbose then
+    io.write("\n################## OUTPUT FILE ###################\n")
+    io.write(table.concat(outputLines))
+    io.write("(END)\n")
+  end
+  
+  for _, line in ipairs(outputLines) do
+    outputFile:write(line)
   end
   
   outputFile:close()
