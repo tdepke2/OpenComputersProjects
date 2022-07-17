d9cec3b451398c0f6eb30fd40c2c1c7bee371213
Thomas Depke <tommydepke@gmail.com>
2022-07-16 18:16:58 -0600

    Fixed tab alignment and a comment.

--- xprint/hexdump.lua
+++ xprint/hexdump.lua
@@ -3,9 +3,9 @@ local xprint = require("xprint")
 local args = {...}
 
 if not args[1] or args[1] == "-" then
-    xprint.hexdump(io.stdin)
+  xprint.hexdump(io.stdin)
 else
-    local file = io.open(args[1], "r")
-    xprint.hexdump(file)
-    file:close()
+  local file = io.open(args[1], "r")
+  xprint.hexdump(file)
+  file:close()
 end
--- xprint/xprint.lua
+++ xprint/xprint.lua
@@ -11,97 +11,97 @@ local xprint = {}
 -- Enables xprint() function call as a shortcut to xprint.print().
 setmetatable(xprint, {
   __call = function(func, ...)
-    return xprint.print(...)
+  return xprint.print(...)
   end
 })
 
 local function writeValueToStream(config, v, stream, endOfLine)
-    local typeV = type(v)
-    if not config.formatHex then
-        if not config.rawString and typeV == "string" then
-            stream:write(string.format("%q", v):gsub("\\\n","\\n"), endOfLine)
-        else
-            stream:write(tostring(v), endOfLine)
-        end
-    elseif typeV == "number" then
-        if math.type and math.type(v) == "integer" then
-            v = string.pack(">l", v)
-        else
-            v = string.pack(">n", v)
-        end
-        stream:write("0x", string.format(string.rep("%.2X", #v), string.byte(v, 1, #v)), endOfLine)
-    elseif not config.rawString and typeV == "string" then
-        stream:write("0x")
-        for i = 1, #v, 100 do
-            local endIndex = math.min(i + 99, #v)
-            stream:write(string.format(string.rep("%.2X ", endIndex - i + 1), string.byte(v, i, endIndex)))
-        end
-        stream:write(endOfLine)
+  local typeV = type(v)
+  if not config.formatHex then
+    if not config.rawString and typeV == "string" then
+      stream:write(string.format("%q", v):gsub("\\\n","\\n"), endOfLine)
     else
-        stream:write(tostring(v), endOfLine)
+      stream:write(tostring(v), endOfLine)
     end
+  elseif typeV == "number" then
+    if math.type and math.type(v) == "integer" then
+      v = string.pack(">l", v)
+    else
+      v = string.pack(">n", v)
+    end
+    stream:write("0x", string.format(string.rep("%.2X", #v), string.byte(v, 1, #v)), endOfLine)
+  elseif not config.rawString and typeV == "string" then
+    stream:write("0x")
+    for i = 1, #v, 100 do
+      local endIndex = math.min(i + 99, #v)
+      stream:write(string.format(string.rep("%.2X ", endIndex - i + 1), string.byte(v, i, endIndex)))
+    end
+    stream:write(endOfLine)
+  else
+    stream:write(tostring(v), endOfLine)
+  end
 end
 
 local function writeTableToStream(config, t, stream, spacing, depth)
-    if config[t] then
-        stream:write(tostring(t), " (duplicate)\n")
-        return
+  if config[t] then
+    stream:write(tostring(t), " (duplicate)\n")
+    return
+  end
+  stream:write(tostring(t), " {\n")
+  config[t] = true
+  
+  local function rawPairs(t)
+    return next, t
+  end
+  
+  if depth <= config.maxDepth then
+    local sortedKeys, stringKeys, otherKeys = {}, {}, {}
+    local iterationMethod = config.rawAccess and rawPairs or pairs
+    for k, v in iterationMethod(t) do
+      if type(k) == "number" then
+        sortedKeys[#sortedKeys + 1] = k
+      elseif type(k) == "string" then
+        stringKeys[#stringKeys + 1] = k
+      else
+        otherKeys[#otherKeys + 1] = k
+      end
     end
-    stream:write(tostring(t), " {\n")
-    config[t] = true
-    
-    local function rawPairs(t)
-        return next, t
+    table.sort(sortedKeys)
+    table.sort(stringKeys)
+    local sortedKeysSize = #sortedKeys
+    for i, v in ipairs(stringKeys) do
+      sortedKeys[sortedKeysSize + i] = v
     end
-    
-    if depth <= config.maxDepth then
-        local sortedKeys, stringKeys, otherKeys = {}, {}, {}
-        local iterationMethod = config.rawAccess and rawPairs or pairs
-        for k, v in iterationMethod(t) do
-            if type(k) == "number" then
-                sortedKeys[#sortedKeys + 1] = k
-            elseif type(k) == "string" then
-                stringKeys[#stringKeys + 1] = k
-            else
-                otherKeys[#otherKeys + 1] = k
-            end
-        end
-        table.sort(sortedKeys)
-        table.sort(stringKeys)
-        local sortedKeysSize = #sortedKeys
-        for i, v in ipairs(stringKeys) do
-            sortedKeys[sortedKeysSize + i] = v
-        end
-        sortedKeysSize = #sortedKeys
-        for i, v in ipairs(otherKeys) do
-            sortedKeys[sortedKeysSize + i] = v
-        end
-        
-        for _, k in ipairs(sortedKeys) do
-            stream:write(spacing, "  ")
-            writeValueToStream(config, k, stream, "")
-            stream:write(": ")
-            if type(t[k]) == "table" then
-                writeTableToStream(config, t[k], stream, spacing .. "  ", depth + 1)
-            else
-                writeValueToStream(config, t[k], stream, "\n")
-            end
-        end
-    elseif next(t) ~= nil then
-        stream:write(spacing, "  ...\n")
+    sortedKeysSize = #sortedKeys
+    for i, v in ipairs(otherKeys) do
+      sortedKeys[sortedKeysSize + i] = v
     end
     
-    if config.hideMeta or not getmetatable(t) then
-        stream:write(spacing, "}\n")
+    for _, k in ipairs(sortedKeys) do
+      stream:write(spacing, "  ")
+      writeValueToStream(config, k, stream, "")
+      stream:write(": ")
+      if type(t[k]) == "table" then
+        writeTableToStream(config, t[k], stream, spacing .. "  ", depth + 1)
+      else
+        writeValueToStream(config, t[k], stream, "\n")
+      end
+    end
+  elseif next(t) ~= nil then
+    stream:write(spacing, "  ...\n")
+  end
+  
+  if config.hideMeta or not getmetatable(t) then
+    stream:write(spacing, "}\n")
+  else
+    stream:write(spacing, "} meta ")
+    local tMeta = getmetatable(t)
+    if type(tMeta) == "table" then
+      writeTableToStream(config, tMeta, stream, spacing, depth)
     else
-        stream:write(spacing, "} meta ")
-        local tMeta = getmetatable(t)
-        if type(tMeta) == "table" then
-            writeTableToStream(config, tMeta, stream, spacing, depth)
-        else
-            writeValueToStream(config, tMeta, stream, "\n")
-        end
+      writeValueToStream(config, tMeta, stream, "\n")
     end
+  end
 end
 
 -- config[t] hides table t
@@ -112,77 +112,78 @@ end
 -- config.rawString disables string escaping
 -- config.formatHex shows values in hex
 function xprint.print(config, ...)
-    assert(type(config) == "table", "config table must be first argument to xprint()")
-    local stream = config.stream or io.stdout
-    config.maxDepth = config.maxDepth or math.huge
-    
-    local arg = table.pack(...)
-    for i = 1, arg.n do
-        if type(arg[i]) == "table" then
-            writeTableToStream(config, arg[i], stream, "", 1)
-        else
-            writeValueToStream(config, arg[i], stream, "\n")
-        end
+  assert(type(config) == "table", "config table must be first argument to xprint()")
+  local stream = config.stream or io.stdout
+  config.maxDepth = config.maxDepth or math.huge
+  
+  local arg = table.pack(...)
+  for i = 1, arg.n do
+    if type(arg[i]) == "table" then
+      writeTableToStream(config, arg[i], stream, "", 1)
+    else
+      writeValueToStream(config, arg[i], stream, "\n")
     end
-    if arg.n == 0 then
-        stream:write("\n")
-    end
+  end
+  if arg.n == 0 then
+    stream:write("\n")
+  end
 end
 
+-- The hexdump format uses the "canonical hex+ASCII display".
 function xprint.hexdump(istream)
-    local address = 0
-    local bytes = istream:read(16)
-    while bytes do
-        io.write(string.format("%.8x  ", address))
-        io.write(string.format(string.rep("%.2x ", math.min(#bytes, 8)), string.byte(bytes, 1, math.min(#bytes, 8))), " ")
-        if #bytes > 8 then
-            io.write(string.format(string.rep("%.2x ", #bytes - 8), string.byte(bytes, 9, #bytes)))
-        end
-        if #bytes < 16 then
-            io.write(string.rep("   ", 16 - #bytes))
-        end
-        io.write(" |", string.gsub(bytes, "[^%g ]", "."), "|\n")
-        address = address + #bytes
-        bytes = istream:read(16)
+  local address = 0
+  local bytes = istream:read(16)
+  while bytes do
+    io.write(string.format("%.8x  ", address))
+    io.write(string.format(string.rep("%.2x ", math.min(#bytes, 8)), string.byte(bytes, 1, math.min(#bytes, 8))), " ")
+    if #bytes > 8 then
+      io.write(string.format(string.rep("%.2x ", #bytes - 8), string.byte(bytes, 9, #bytes)))
     end
-    if address ~= 0 then
-        io.write(string.format("%.8x  ", address), "\n")
+    if #bytes < 16 then
+      io.write(string.rep("   ", 16 - #bytes))
     end
+    io.write(" |", string.gsub(bytes, "[^%g ]", "."), "|\n")
+    address = address + #bytes
+    bytes = istream:read(16)
+  end
+  if address ~= 0 then
+    io.write(string.format("%.8x  ", address), "\n")
+  end
 end
 
 
 local t1 = {
-    abc = 123,
-    [1] = "first",
-    [2] = "sec",
-    [3] = "third",
-    my = -1,
-    sample = -2,
-    text = -3,
-    here = -4,
-    [{}] = -5,
-    [function() end] = "cooler",
-    [ [["! level2
-        ]] ] = setmetatable({
-        "a",
-        "b",
-        "c",
-        thing = "cool",
-        [function() end] = {
-            __index = " ",
-            __metatable = 123.456,
-        },
-    }, {__index = function() print("ok") end, [3] = "test"}),
-    hidden = setmetatable({
-        "not here",
-        123,
-        function() return 6 end,
-        "secret ",
-    }, {__pairs = function() return function() end end, __metatable = "you can\'t see my secret pairs func!"}),
+  abc = 123,
+  [1] = "first",
+  [2] = "sec",
+  [3] = "third",
+  my = -1,
+  sample = -2,
+  text = -3,
+  here = -4,
+  [{}] = -5,
+  [function() end] = "cooler",
+  [ [["! level2
+    ]] ] = setmetatable({
+    "a",
+    "b",
+    "c",
+    thing = "cool",
+    [function() end] = {
+      __index = " ",
+      __metatable = 123.456,
+    },
+  }, {__index = function() print("ok") end, [3] = "test"}),
+  hidden = setmetatable({
+    "not here",
+    123,
+    function() return 6 end,
+    "secret ",
+  }, {__pairs = function() return function() end end, __metatable = "you can\'t see my secret pairs func!"}),
 }
 t1[11] = t1
 setmetatable(t1, getmetatable(t1[ [["! level2
-        ]] ]))
+    ]] ]))
 
 
 --xprint({}, nil, 12.345, "test\ncool  ", t1, t1, function() end)
