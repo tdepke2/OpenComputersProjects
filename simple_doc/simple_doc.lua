04dff7fdd1851f54e6e8e19d91b66f4d5bacfecc
Thomas Depke <tommydepke@gmail.com>
2022-08-08 23:57:32 -0600

    Added simple_doc for easily automating documentation creation.

---
+++ simple_doc/simple_doc.lua
@@ -0,0 +1,239 @@
+--------------------------------------------------------------------------------
+-- Lua documentation generator.
+-- 
+-- @see file://simple_doc/README.md
+-- @author tdepke2
+--------------------------------------------------------------------------------
+
+
+local fs = require("filesystem")
+local shell = require("shell")
+
+local USAGE_STRING = [[Usage: simple_doc [OPTION]... [INPUT-FILE] [OUTPUT-FILE]
+
+Options:
+  -h, --help                   display help message and exit
+  -B, --boilerplate=NUMBER     ignore first N comments in file (boilerplate comments)
+  -C, --context                include next line in file after each comment block
+      --insert-start=STRING    insert output text in file starting at the given string
+      --insert-end=STRING      same as insert-start for the ending position
+
+For more information, run: man simple_doc
+]]
+
+local args, opts = shell.parse(...)
+
+
+-- Check if a file was opened successfully, and raise an error if not.
+local function checkFileError(filename, file, err)
+  if not file then
+    if fs.isDirectory(filename) then
+      io.stderr:write("simple_doc " .. filename .. ": is a directory\n")
+    else
+      io.stderr:write("simple_doc " .. filename .. ": " .. tostring(err) .. "\n")
+    end
+    os.exit(2)
+  end
+end
+
+
+local function writeSection(outputFile, docSection)
+  if docSection.n == 0 then
+    return
+  end
+  if opts["B"] and docSection.sectionNumber <= opts["B"] then
+    for i, v in ipairs(docSection) do
+      docSection[i] = nil
+    end
+  else
+    if opts["B"] then
+      if docSection.sectionNumber > opts["B"] + 1 then
+        outputFile:write("\n")
+      end
+    elseif docSection.sectionNumber ~= 1 then
+      outputFile:write("\n")
+    end
+    -- Trim trailing lines that are only whitespace.
+    for i = docSection.n, 1, -1 do
+      if string.find(docSection[i], "%S") then
+        break
+      end
+      docSection[i] = nil
+    end
+    -- Add remaining lines to output.
+    for i, v in ipairs(docSection) do
+      outputFile:write("[" .. v .. "]\n")
+      docSection[i] = nil
+    end
+  end
+  docSection.sectionNumber = docSection.sectionNumber + 1
+  docSection.n = 0
+end
+
+
+local function buildDoc(inputFile, outputFile)
+  local state = 0
+  local docSection = {sectionNumber = 1, n = 0}
+  
+  local line, lineNum = inputFile:read(), 1
+  while line do
+    if state == 1 then
+      -- Within comment block (line type).
+      local docText = string.match(line, "%s*%-%-+%s?(.*)")
+      if docText then
+        docSection.n = docSection.n + 1
+        docSection[docSection.n] = docText
+      else
+        if opts["C"] then
+          docSection.n = docSection.n + 1
+          docSection[docSection.n] = line
+        end
+        state = 0
+        writeSection(outputFile, docSection)
+      end
+    elseif state == 2 then
+      -- Within comment block (multi-line type).
+      if not string.find(line, "%]%]") then
+        docSection.n = docSection.n + 1
+        docSection[docSection.n] = line
+      elseif not opts["C"] then
+        state = 0
+        writeSection(outputFile, docSection)
+      else
+        state = 3
+      end
+    elseif state == 3 then
+      -- At line after a multi-line comment block.
+      docSection.n = docSection.n + 1
+      docSection[docSection.n] = line
+      state = 0
+      writeSection(outputFile, docSection)
+    end
+    
+    -- Default state, check for comment prefix that could indicate doc comment.
+    if state == 0 and string.find(line, "%s*%-%-") then
+      local docText = string.match(line, "%s*%-%-%-+%s?(.*)")
+      if docText then
+        state = 1
+      else
+        docText = string.match(line, "%s*%-%-%[%[%-%-+%s?(.*)")
+        if docText then
+          state = 2
+        end
+      end
+      if docText and #docText > 0 then
+        docSection.n = docSection.n + 1
+        docSection[docSection.n] = docText
+      end
+    end
+    line, lineNum = inputFile:read(), lineNum + 1
+  end
+  writeSection(outputFile, docSection)
+end
+
+
+-- Check command line options, open files, generate documentation, and write
+-- results to output.
+local function main()
+  local inputFilename = (args[1] == nil and "-" or args[1])
+  local outputFilename = (args[2] == nil and "-" or args[2])
+  if #args > 2 or opts["h"] or opts["help"] then
+    io.write(USAGE_STRING)
+    os.exit(0)
+  end
+  
+  if opts["boilerplate"] then
+    opts["B"] = opts["boilerplate"]
+  end
+  if opts["B"] then
+    opts["B"] = math.max(tonumber(opts["B"]) or 1, 0)
+  end
+  
+  if opts["context"] then
+    opts["C"] = true
+  end
+  
+  if opts["insert-start"] or opts["insert-end"] then
+    if type(opts["insert-start"]) ~= "string" or type(opts["insert-end"]) ~= "string" then
+      io.stderr:write("simple_doc: must provide both --insert-start and --insert-end with string values, or skip them both\n")
+      os.exit(2)
+    end
+  end
+  
+  local inputFile, outputFile, err
+  local outputFileContents
+  
+  -- Attempt to open input file (or use stdin).
+  if inputFilename == "-" then
+    inputFile, err = io.stdin, "missing stdin"
+  else
+    inputFilename = shell.resolve(inputFilename)
+    inputFile, err = io.open(inputFilename)
+  end
+  checkFileError(inputFilename, inputFile, err)
+  
+  -- Attempt to open output file (or use stdout).
+  if outputFilename == "-" then
+    outputFile, err = io.stdout, "missing stdout"
+  else
+    outputFilename = shell.resolve(outputFilename)
+    if opts["insert-start"] then
+      outputFile, err = io.open(outputFilename)
+      checkFileError(outputFilename, outputFile, err)
+      
+      local startFound, endFound
+      outputFileContents = {n = 0, insertIndex = 0}
+      local line = outputFile:read()
+      while line do
+        if not startFound then
+          if line == opts["insert-start"] then
+            startFound = true
+            outputFileContents.insertIndex = outputFileContents.n + 1
+          end
+          outputFileContents.n = outputFileContents.n + 1
+          outputFileContents[outputFileContents.n] = line
+        elseif not endFound then
+          if line == opts["insert-end"] then
+            endFound = true
+            outputFileContents.n = outputFileContents.n + 1
+            outputFileContents[outputFileContents.n] = line
+          end
+        else
+          outputFileContents.n = outputFileContents.n + 1
+          outputFileContents[outputFileContents.n] = line
+        end
+        line = outputFile:read()
+      end
+      
+      if not startFound then
+        io.stderr:write("simple_doc " .. outputFilename .. ": failed to find insert-start string \"" .. opts["insert-start"] .. "\" in file\n")
+        os.exit(2)
+      end
+      if not endFound then
+        io.stderr:write("simple_doc " .. outputFilename .. ": failed to find insert-end string \"" .. opts["insert-end"] .. "\" in file\n")
+        os.exit(2)
+      end
+      outputFile:close()
+    end
+    outputFile, err = io.open(outputFilename, "w")
+  end
+  checkFileError(outputFilename, outputFile, err)
+  
+  if outputFileContents then
+    for i = 1, outputFileContents.insertIndex do
+      outputFile:write(outputFileContents[i], "\n")
+    end
+  end
+  
+  buildDoc(inputFile, outputFile)
+  inputFile:close()
+  
+  if outputFileContents then
+    for i = outputFileContents.insertIndex + 1, outputFileContents.n do
+      outputFile:write(outputFileContents[i], "\n")
+    end
+  end
+  outputFile:close()
+end
+
+main()
