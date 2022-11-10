fadbe9f39e79cf33014ab5c1ca4ad3ef09e39723
Thomas Depke <tommydepke@gmail.com>
2022-11-09 23:29:42 -0700

    Updated some comments, and added partial implementation of config.saveFile().

--- libconfig/config.lua
+++ libconfig/config.lua
@@ -309,27 +309,32 @@ function config.loadFile(filename, cfgFormat, allowDefaults, localEnv)
   return cfg
 end
 
-function config.saveFile()
-  
-end
-
-local function verifyType(value, typeNames, address)
+-- Compares the given value with the string typeNames of acceptable types. If no
+-- match is found for any of the types, this function throws an error with the
+-- address where it occurred. Otherwise, returns the first name in typeNames
+-- that matched the value type.
+-- 
+---@param value any
+---@param typeNames string
+---@param valueName string
+---@param address string
+---@return string typeVerified
+local function verifyType(value, typeNames, valueName, address)
   -- Iterate type names (separated by vertical bars) until we find a match with value.
-  local typeVerified = false
-  local typeCheckError
+  local typeVerified, typeCheckError
   for typeName in string.gmatch(typeNames, "[%w_]+") do
     if typeName == "any" then
-      typeVerified = true
+      typeVerified = typeName
       break
     elseif luaTypes[typeName] then
       if type(value) == typeName then
-        typeVerified = true
+        typeVerified = typeName
         break
       end
     elseif typeList[typeName] then
       local status, result = pcall(typeList[typeName].verify or function() end, value)
       if status then
-        typeVerified = true
+        typeVerified = typeName
         break
       else
         typeCheckError = result
@@ -340,16 +345,19 @@ local function verifyType(value, typeNames, address)
   end
   
   if typeCheckError then
-    error("at \"" .. address .. "\": bad value: " .. tostring(typeCheckError))
+    error("at \"" .. address .. "\": bad " .. valueName .. ": " .. tostring(typeCheckError))
   elseif not typeVerified then
-    error("at \"" .. address .. "\": value does not match any of the allowed types \"" .. typeNames .. "\".")
+    error("at \"" .. address .. "\": " .. valueName .. " does not match any of the allowed types \"" .. typeNames .. "\".")
   end
-  
-  
-  -- FIXME we call this function with keys too, it shouldn't always say "value". should we print the value in the error or nah? ##########################
-  
+  return typeVerified
 end
 
+-- Helper function to concatenate the next key onto the address, using a dot or
+-- brackets where appropriate.
+-- 
+---@param address string
+---@param key any
+---@return string
 local function nextAddress(address, key)
   if type(key) == "string" then
     return address .. "." .. key
@@ -358,17 +366,25 @@ local function nextAddress(address, key)
   end
 end
 
+-- Helper function for `config.verify()` to check for errors in configuration.
+-- 
+---@param cfg table
+---@param cfgFormat table
+---@param typeList table
+---@param address string
 local function verifySubconfig(cfg, cfgFormat, typeList, address)
-  assert(type(cfgFormat) == "table", "bad2")
+  if type(cfgFormat) ~= "table" then
+    error("at \"" .. address .. "\": expected table in configuration format.")
+  end
   
   -- If first index in cfgFormat is a string, the table represents a value definition.
   -- Value format: `{<type names>, [default value], [comment]}`.
   if type(cfgFormat[1]) == "string" then
-    verifyType(cfg, cfgFormat[1], address)
+    verifyType(cfg, cfgFormat[1], "value", address)
     return
   end
   
-  -- 
+  -- Match keys in cfgFormat with ones in cfg (format fields are skipped). We mark these as processed so they won't get picked up a second time in "_ipairs_" or "_pairs_".
   local processedKeys = {}
   for k, v in pairs(cfgFormat) do
     if not formatFields[k] then
@@ -380,13 +396,14 @@ local function verifySubconfig(cfg, cfgFormat, typeList, address)
     end
   end
   
+  -- Check for "_ipairs" second and iterate sequential keys in cfg.
   if cfgFormat._ipairs_ then
     local valueTypes = cfgFormat._ipairs_[1]
     for i, v in ipairs(cfg) do
       if not processedKeys[i] then
         processedKeys[i] = true
         if type(valueTypes) == "string" then
-          verifyType(v, valueTypes, nextAddress(address, i))
+          verifyType(v, valueTypes, "value", nextAddress(address, i))
         else
           verifySubconfig(v, valueTypes, typeList, nextAddress(address, i))
         end
@@ -394,15 +411,16 @@ local function verifySubconfig(cfg, cfgFormat, typeList, address)
     end
   end
   
+  -- Check for "_pairs" third and iterate remaining keys/values in cfg.
   if cfgFormat._pairs_ then
     local keyTypes, valueTypes = cfgFormat._pairs_[1], cfgFormat._pairs_[2]
     for k, v in pairs(cfg) do
       if not processedKeys[k] then
         local address2 = nextAddress(address, k)
         processedKeys[k] = true
-        verifyType(k, keyTypes, address2)
+        verifyType(k, keyTypes, "key", address2)
         if type(valueTypes) == "string" then
-          verifyType(v, valueTypes, address2)
+          verifyType(v, valueTypes, "value", address2)
         else
           verifySubconfig(v, valueTypes, typeList, address2)
         end
@@ -410,6 +428,7 @@ local function verifySubconfig(cfg, cfgFormat, typeList, address)
     end
   end
   
+  -- Confirm no extra fields are defined in cfg that we didn't match previously.
   for k, v in pairs(cfg) do
     if not processedKeys[k] then
       error("at \"" .. nextAddress(address, k) .. "\": key is undefined in configuration format.")
@@ -417,9 +436,156 @@ local function verifySubconfig(cfg, cfgFormat, typeList, address)
   end
 end
 
+-- Checks the format of config cfg to make sure it matches cfgFormat. An error
+-- is thrown if any inconsistencies with the format are found.
+-- 
+---@param cfg table
+---@param cfgFormat table
+---@param typeList table
 function config.verify(cfg, cfgFormat, typeList)
   verifySubconfig(cfg, cfgFormat, typeList, "config")
 end
+
+
+
+local luaReservedWords = {
+  ["and"] = true,
+  ["break"] = true,
+  ["do"] = true,
+  ["else"] = true,
+  ["elseif"] = true,
+  ["end"] = true,
+  ["false"] = true,
+  ["for"] = true,
+  ["function"] = true,
+  ["goto"] = true,
+  ["if"] = true,
+  ["in"] = true,
+  ["local"] = true,
+  ["nil"] = true,
+  ["not"] = true,
+  ["or"] = true,
+  ["repeat"] = true,
+  ["return"] = true,
+  ["then"] = true,
+  ["true"] = true,
+  ["until"] = true,
+  ["while"] = true,
+}
+
+local id = "^[%a_][%w_]*$"
+
+
+
+local function writeValue()
+  
+end
+
+
+-- FIXME this is very similar to verification code, can we merge the two somehow? #############################################################
+local function writeSubconfig(file, cfg, cfgFormat, typeList, address)
+  if type(cfgFormat) ~= "table" then
+    error("at \"" .. address .. "\": expected table in configuration format.")
+  end
+  
+  if type(cfgFormat[1]) == "string" then
+    local typeVerified = verifyType(cfg, cfgFormat[1], "value", address)
+    if typeList[typeVerified] and typeList[typeVerified].tostring then
+      local status, result = pcall(typeList[typeVerified].tostring, cfg)
+      if not status or type(result) ~= "string" then
+        if status then
+          result = "result is type " .. type(result)
+        end
+        error("at \"" .. address .. "\": failed to convert " .. typeVerified .. " to string: " .. tostring(result))
+      end
+      file:write(result, "\n")
+    elseif false then --FIXME #################  type(cfg) == "table" then
+      
+      -- need to recursively print the table
+      
+    else
+      file:write(tostring(cfg), "\n")
+    end
+    return
+  end
+  
+  local customOrder = {}
+  for k, v in pairs(cfgFormat) do
+    if type(v) == "table" and v._order_ then
+      customOrder[v._order_] = k
+    end
+  end
+  
+  
+  -- Collect all table keys and sort them. Based on code used in xprint module.
+  local sortedKeys, stringKeys, otherKeys = {}, {}, {}
+  for k, v in pairs(cfg) do
+    if type(k) == "number" then
+      sortedKeys[#sortedKeys + 1] = k
+    elseif type(k) == "string" then
+      stringKeys[#stringKeys + 1] = k
+    else
+      otherKeys[#otherKeys + 1] = k
+    end
+  end
+  table.sort(sortedKeys)
+  table.sort(stringKeys)
+  local sortedKeysSize = #sortedKeys
+  for i, v in ipairs(stringKeys) do
+    sortedKeys[sortedKeysSize + i] = v
+  end
+  sortedKeysSize = #sortedKeys
+  for i, v in ipairs(otherKeys) do
+    sortedKeys[sortedKeysSize + i] = v
+  end
+  
+  
+  
+  
+  -- left off here, need to iterate keys in the sortedKeys order, and factor in customOrder if provided
+  
+  
+  
+  
+  local processedKeys = {}
+  for k, v in pairs(cfgFormat) do
+    if not formatFields[k] then
+      if cfg[k] == nil then
+        error("at \"" .. nextAddress(address, k) .. "\": key must be provided in configuration.")
+      end
+      if type(v) == "table" and v._comment_ then
+        file:write("-- ", v._comment_, "\n")    -- FIXME need better comment handling. #######################
+      end
+      processedKeys[k] = true
+      writeSubconfig(file, cfg[k], v, typeList, nextAddress(address, k))
+    end
+  end
+  
+  
+  
+  
+end
+
+
+-- FIXME still need to implement fromstring() handling in typeList. #########################################################
+
+
+
+-- 
+-- 
+---@param filename string
+---@param cfg table
+---@param cfgFormat table
+---@param typeList table
+function config.saveFile(filename, cfg, cfgFormat, typeList)
+  --local file = io.open(filename, "w")
+  --if not file then
+    --error("failed to open file \"" .. filename .. "\" for writing.")
+  --end
+  
+  writeSubconfig(io.stdout, cfg, cfgFormat, typeList, "config")
+end
+
 
 --local xprint = require("xprint")
 local cfg = config.loadFile("/home/configTest2", cfgFormat, true)
